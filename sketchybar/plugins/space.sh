c2911f25f39e2e318cf3f7240e6b2ec8be477cb9
Ken Banks <kenbanks.peng@gmail.com>
2024-07-23 01:34:16 -0400

    .

--- sketchybar/plugins/space.sh
+++ sketchybar/plugins/space.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
-# The $SELECTED variable is available for space components and indicates if
-# the space invoking this script (with name: $NAME) is currently selected:
-# https://felixkratz.github.io/SketchyBar/config/components#space----associate-mission-control-spaces-with-an-item
+# The $SELECTED variable is available for space components
 
 source "$CONFIG_DIR/colors.sh" # Loads all defined colors
 
