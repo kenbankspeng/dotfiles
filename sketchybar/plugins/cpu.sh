9fab6f47a285b8b6c9e57b96fcf3b8bae6b0bc07
Ken Banks <kenbanks.peng@gmail.com>
2024-07-23 06:09:21 -0400

    fix

--- sketchybar/plugins/cpu.sh
+++ sketchybar/plugins/cpu.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
-
-sketchybar -m --set "$NAME" \
-  label="$(top -l 2 | grep -E "^CPU" | tail -1 | awk '{ print int($3 + $5 + 0.5)"%" }')" \
-  label.font="SF Pro:Semibold:12"
+cpu="$(top -l 2 | grep -E "^CPU" | tail -1 | awk '{ print int($3 + $5 + 0.5)"%" }')"
+if [ -n "$cpu" ]; then
+  sketchybar -m --set "$NAME" \
+    label="$cpu" \
+    label.font="SF Pro:Semibold:12"
+fi
