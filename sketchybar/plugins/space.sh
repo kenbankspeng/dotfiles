e1ca5c493ec99f7c64c06d9fb46c3099024fe253
Ken Banks <kenbanks.peng@gmail.com>
2024-07-24 02:16:52 -0400

    color

--- sketchybar/plugins/space.sh
+++ sketchybar/plugins/space.sh
@@ -1,5 +1,22 @@
 #!/usr/bin/env bash
 
+# modify alpha of a 0xaarrggbb color
+alpha() {
+  local color=$1
+  local new_alpha=$2
+
+  # Extract the remaining part of the color (rrggbb)
+  local rgb_part=${color:4}
+
+  echo 0x${new_alpha}${rgb_part} # new color
+}
+
+# Example usage
+original_color=0xff123456
+new_alpha=80
+
+new_color=$(change_alpha $original_color $new_alpha)
+
 # The $SELECTED variable is available for space components
 
 source "$CONFIG_DIR/colors.sh"
@@ -9,13 +26,15 @@ index=$(echo "$NAME" | awk -F. '{print $2}')
 accent=$(eval echo \$ACCENT$index)
 
 if [ $SELECTED = true ]; then
-  border=$accent
+  border=$(alpha $accent 80) # 80 hex
+  bg=$(alpha $accent 40)     # 40 hex
 else
   border=$TRANSPARENT
+  bg=$TRANSPARENT
 fi
 
 props=(
-  # background.color=$bg
+  background.color=$bg
   background.border_width=1
   background.border_color=$border
   label.color=$accent
