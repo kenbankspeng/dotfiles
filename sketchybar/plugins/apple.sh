488e78b88e8c21dc299263d6ebaceae231d7af06
Ken Banks <kenbanks.peng@gmail.com>
2024-07-26 20:27:51 -0400

    Noice!

--- sketchybar/plugins/apple.sh
+++ sketchybar/plugins/apple.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
 
-open "raycast://extensions/raycast/raycast/confetti"
+confetti() {
+  afplay "$CONFIG_DIR/plugins/helpers/Cheer.wav" &
+  open "raycast://extensions/raycast/raycast/confetti" &
+}
+
+if [ "$BUTTON" == "left" ]; then
+  confetti
+elif [ "$BUTTON" == "right" ]; then
+  confetti
+fi
