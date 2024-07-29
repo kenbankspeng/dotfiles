8863bd2c58e1387ab7ba056d284a94a32933812e
Ken Banks <kenbanks.peng@gmail.com>
2024-07-29 04:30:10 -0400

    .

--- sketchybar/items/windows.sh
+++ sketchybar/items/windows.sh
@@ -7,6 +7,3 @@ sketchybar --add event window_changed \
   --add item windows left \
   --set windows script="$PLUGIN_DIR/windows.sh" \
   --subscribe windows window_changed
-
-# sketchybar --add bracket spaces '/window\..*/' \
-#   --set spaces background.color=red
