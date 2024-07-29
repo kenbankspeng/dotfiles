05c8b34aa02d17a19af8295847030ed3340bffc9
Ken Banks <kenbanks.peng@gmail.com>
2024-07-29 02:53:56 -0400

    close

--- sketchybar/items/windows.sh
+++ sketchybar/items/windows.sh
@@ -1,22 +1,9 @@
-wid=0
-windows=()
-for i in {1..20}; do
-  wid=$i
-
-  props=(
-    padding_left=5
-    padding_right=5
-    label.drawing=off
-    icon.font="$SKETCHY_FONT:$SKETCHY_FONTSIZE"
-    icon.padding_left=2
-    icon.padding_right=2
-  )
-  sketchybar --add item window.$wid left \
-    --set window.$wid "${props[@]}"
-  #--subscribe window.$sid mouse.clicked
-done
+#!/usr/bin/env bash
 
 sketchybar --add event window_changed \
   --add item windows left \
   --set windows script="$PLUGIN_DIR/windows.sh" \
   --subscribe windows window_changed
+
+# sketchybar --add bracket spaces '/window\..*/' \
+#   --set spaces background.color=red
--- sketchybar/plugins/windows.sh
+++ sketchybar/plugins/windows.sh
@@ -1,20 +1,45 @@
 #!/usr/bin/env bash
 
-# TODO: front_app_switched
+source "$CONFIG_DIR/colors.sh"
 
+props=(
+  label.drawing=off
+  icon.drawing=off
+  padding_left=5
+  padding_right=5
+  icon.font="$SKETCHY_FONT:$SKETCHY_FONTSIZE"
+  icon.padding_left=2
+  icon.padding_right=2
+)
+
+# Get dynamic window information grouped by space
 query=$(yabai -m query --windows | jq '[group_by(.space)[] | {
     space: .[0].space,
     apps: [.[] | .app],
     has_focus: (map(select(.["has-focus"] == true).app) | if length > 0 then .[0] else false end)
 }]')
 
-wid=0
+# Create and update windows dynamically
 echo $query | jq -c '.[]' | while IFS= read -r item; do
-  space=$(echo "$item" | jq '.space')
-  apps=$(echo "$item" | jq -r '.apps[]')
-  for app in $apps; do
-    wid=$(($wid + 1))
+  sid=$(echo "$item" | jq '.space')
+  apps=($(echo "$item" | jq -r '.apps[]'))
+
+  # Allocate only needed items
+  for ((wid = 0; wid < ${#apps[@]}; wid++)); do
+    app=${apps[$wid]}
     icon="$($CONFIG_DIR/icon_map.sh "$app")"
-    sketchybar --set window.$wid icon="$icon"
+    sketchybar --add item window.$sid.$wid left \
+      --set window.$sid.$wid "${props[@]}" icon.drawing=on icon="$icon"
+  done
+
+  # Remove any excess items dynamically
+  wid=${#apps[@]}
+  while true; do
+    if sketchybar --query window.$sid.$wid &>/dev/null; then
+      sketchybar --remove window.$sid.$wid
+      ((wid++))
+    else
+      break
+    fi
   done
 done
