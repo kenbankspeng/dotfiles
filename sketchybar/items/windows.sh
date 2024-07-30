3b9c75703a99422a81e0038b863e4bb55e537908
Ken Banks <kenbanks.peng@gmail.com>
2024-07-30 04:13:49 -0400

    .

--- sketchybar/items/windows.sh
+++ sketchybar/items/windows.sh
@@ -3,7 +3,13 @@
 # this cache is cleared on every startup so windows get created
 rm -rf "$CACHE_DIR"
 
+windows_props=(
+  label.drawing=off
+  icon.drawing=off
+  background.drawing=off
+  script="$PLUGIN_DIR/windows.sh"
+)
 sketchybar --add event window_changed \
   --add item windows left \
-  --set windows script="$PLUGIN_DIR/windows.sh" \
+  --set windows "${windows_props[@]}" \
   --subscribe windows window_changed
