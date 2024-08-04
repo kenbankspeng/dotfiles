9c763a92a33e5572b12c3854c34cd55aae44d6a2
Ken Banks <kenbanks.peng@gmail.com>
2024-08-03 23:16:59 -0400

    .

--- sketchybar/items/stack.sh
+++ sketchybar/items/stack.sh
@@ -1,33 +1,37 @@
 #!/usr/bin/env bash
 
+position=q
+
 props=(
+  label.drawing=off
   icon.font="$ICON_FONT:$ICON_FONTSIZE"
   icon.color=$GREY
   script="$PLUGIN_DIR/stack.sh"
 )
 
 # stack
-sketchybar -m --add item stack q \
+sketchybar -m --add item stack $position \
   --set stack icon=􀏧 ${props[@]} \
   --subscribe stack mouse.clicked
 
 # bsp = grid
-sketchybar -m --add item bsp q \
+sketchybar -m --add item bsp $position \
   --set bsp icon=􀏝 ${props[@]} \
   --subscribe bsp mouse.clicked
 
 # float = normal
-sketchybar -m --add item float q \
+sketchybar -m --add item float $position \
   --set float icon=􀆨 ${props[@]} \
   --subscribe float mouse.clicked
 
 props=(
   icon.drawing=off
   label.drawing=off
+  padding_left=20
   background.color=off
   script="$PLUGIN_DIR/stack.sh"
 )
 sketchybar --add event window_changed \
-  --add item stack_listener left \
+  --add item stack_listener $position \
   --set stack_listener "${props[@]}" \
   --subscribe stack_listener window_changed
