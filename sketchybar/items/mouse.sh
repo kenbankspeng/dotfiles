ccc10a5a5da969b9c8df2de492444cae8e65ea45
Ken Banks <kenbanks.peng@gmail.com>
2024-08-04 00:00:54 -0400

    .

---
+++ sketchybar/items/mouse.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+position=q
+
+props=(
+  label.drawing=off
+  icon.font="$ICON_FONT:$ICON_FONTSIZE"
+  script="$PLUGIN_DIR/mouse.sh"
+)
+
+# stack
+sketchybar -m --add item mouse $position \
+  --set mouse icon=􀇰 ${props[@]} \
+  --subscribe mouse mouse.clicked
