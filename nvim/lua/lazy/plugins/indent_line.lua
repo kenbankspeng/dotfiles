a4e8ab6e11e967eac491f0f44b73bf4788702b1c
Ken Banks <kenbanks.peng@gmail.com>
2024-08-08 19:29:43 -0400

    .

--- nvim/lua/lazy/plugins/indent_line.lua
+++ nvim/lua/lazy/plugins/indent_line.lua
@@ -1,9 +1,15 @@
+vim.cmd [[highlight IndentBlanklineScope guifg=#FF0000]] -- Change #FF0000 to your desired color
+
 return {
   { -- Add indentation guides even on blank lines
     'lukas-reineke/indent-blankline.nvim',
     -- Enable `lukas-reineke/indent-blankline.nvim`
     -- See `:help ibl`
     main = 'ibl',
-    opts = {},
+
+    opts = {
+      indent = { char = '│' },
+      scope = { char = '│' },
+    },
   },
 }
