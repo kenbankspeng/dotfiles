19c345abc86e1d403bd2616b499b4b1fdf87a2c2
Ken Banks <kenbanks.peng@gmail.com>
2024-08-09 05:03:39 -0400

    lazyvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,20 +1,2 @@
---[[
-
-Kickstart Guide:
-  - :Tutor
-  - read this guide
-  - run AND READ `:help` - keymap "<space>sh"
-  - run `:checkhealth`
-  - To check the current status of your plugins, run `:Lazy`
-  - You can press `?` in this menu for help. Use `:q` to close the window
-  - To update plugins you can run `:Lazy update`
-
---]]
-
-require("config.options")
-require("config.keymaps")
-require("config.autocommands")
-require("lazy.lazy")
-
--- The line beneath this is called `modeline`. See `:help modeline`
--- vim: ts=2 sts=2 sw=2 et
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
