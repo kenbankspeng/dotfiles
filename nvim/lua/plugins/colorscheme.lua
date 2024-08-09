ee7f3b87ce6fbf18c299b4e57e001212ee0899e2
Ken Banks <kenbanks.peng@gmail.com>
2024-08-09 07:13:38 -0400

    lazy-vim

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    -- use `:Telescope colorscheme` to see all available colorschemes.
+    'catppuccin/nvim',
+    priority = 1000, -- Make sure to load this before all the other start plugins.
+    init = function()
+      vim.cmd.colorscheme 'catppuccin-mocha'
+
+      -- You can configure highlights by doing something like:
+      vim.cmd.hi 'Comment gui=none'
+    end,
+  }
+}
