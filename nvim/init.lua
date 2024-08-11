ea9a9c4e095cfd2dadac7fd622bf979be058cdff
Ken Banks <kenbanks.peng@gmail.com>
2024-08-11 02:52:10 -0400

    .

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,10 @@
+-- local deleteKeys = require('config.helpers.deleteKeys')
+-- deleteKeys('n')
+
+
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+
+-- local logKeys = require('config.helpers.logKeys')
+-- logKeys('n', '/Users/ken/Documents/Software/DevBox/dotfiles/nvim/lua/debug.txt')
