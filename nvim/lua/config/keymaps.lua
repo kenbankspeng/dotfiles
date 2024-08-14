9454a22bce9816eb3357946db06151bedd24f5fe
Ken Banks <kenbanks.peng@gmail.com>
2024-08-13 20:06:55 -0400

    harpoon2

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -42,6 +42,11 @@ end
 
 -- NVIM/LAZY DEFAULTS -- shown in comments
 
+
+
+
+
+
 -- OIL --
 map("<leader><leader>", "<cmd>Oil<CR>", "open parent directory") -- ok
 
@@ -104,6 +109,18 @@ map("<leader>yy", "<cmd>Yazi cwd<CR>", "Open yazi at working directory") -- ok
 -- <leader><Tab>f    -- buggy? <Cmd>tabfirst<CR>                                  First Tab
 -- <leader><Tab>o    -- buggy? <Cmd>tabonly<CR>                                   Close Other Tabs
 -- <leader><Tab>l    -- buggy? <Cmd>tablast<CR>                                   Last Tab
+
+
+local harpoon = require("harpoon")
+map("<leader>ha", function() harpoon:list():add() end, "harpoon add")
+map("<leader>hl", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, "harpoon list")
+map("<leader>h1", function() harpoon:list():select(1) end, "harpoon select1")
+map("<leader>h2", function() harpoon:list():select(2) end, "harpoon select2")
+map("<leader>h3", function() harpoon:list():select(3) end, "harpoon select")
+map("<leader>h4", function() harpoon:list():select(4) end, "harpoon select")
+map("<leader>hp", function() harpoon:list():prev() end, "harpoon prev")
+map("<leader>hn", function() harpoon:list():next() end, "harpoon next")
+
 
 -- SEARCH -- ok
 -- ?   -- ok
