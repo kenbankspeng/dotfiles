f6cd5b29824d68efdd40712bcf37317a6a6d5d02
Ken Banks <kenbanks.peng@gmail.com>
2024-08-16 08:11:21 -0400

    works

--- wezterm/config/wez_to_nvim.lua
+++ wezterm/config/wez_to_nvim.lua
@@ -22,7 +22,7 @@ local direction_keys = {
 local function split_nav(resize_or_move, key)
   return {
     key = key,
-    mods = resize_or_move == "resize" and "META" or "CTRL",
+    mods = resize_or_move == "resize" and "META" or "SHIFT",
     action = wezterm.action_callback(function(win, pane)
       if is_vim(pane) then
         -- pass the keys through to vim/nvim
