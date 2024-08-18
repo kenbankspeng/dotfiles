50dd128713917840965e80c5e2024da01c2e000e
Ken Banks <kenbanks.peng@gmail.com>
2024-08-18 03:19:40 -0400

    WinSeparator

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -7,8 +7,23 @@ return {
     init = function()
       vim.cmd.colorscheme 'catppuccin-mocha'
 
-      -- You can configure highlights by doing something like:
-      vim.cmd.hi 'Comment gui=none'
+      --
+      -- put custom gui formatting here inside a defer_fn
+      --
+
+      -- REFERENCE:
+      -- rosewater: #f5e0dc,  flamingo: #f2cdcd,  pink: #f5c2e7,
+      -- mauve: #cba6f7,  red: #f38ba8,  maroon: #eba0ac,  peach: #fab387,
+      -- yellow: #f9e2af,  green: #a6e3a1,  teal: #94e2d5,  sky: #89dceb,
+      -- sapphire: #74c7ec,  blue: #89b4fa,  lavender: #b4befe,
+      -- text: #cdd6f4,  subtext1: #bac2de,  subtext0: #a6adc8,
+      -- overlay2: #9399b2,  overlay1: #7f849c,  overlay0: #6c7086,
+      -- surface2: #585b70,  surface1: #45475a,  surface0: #313244,
+      -- base: #1e1e2e,  mantle: #181825,  crust: #11111b,
+
+      vim.defer_fn(function()
+        vim.api.nvim_set_hl(0, 'WinSeparator', { fg = '#255565' })
+      end, 0)
     end,
   }
 }
