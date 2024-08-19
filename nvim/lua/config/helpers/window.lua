f34e46e295e995a09ee962ffd7b34756809d2c15
Ken Banks <kenbanks.peng@gmail.com>
2024-08-18 22:59:25 -0400

    .

--- nvim/lua/config/helpers/window.lua
+++ nvim/lua/config/helpers/window.lua
@@ -3,10 +3,23 @@
 local function smart_close()
   local buf = vim.api.nvim_get_current_buf()
   local wins = vim.fn.win_findbuf(buf)
-  if #wins > 1 then
+  local num_windows = #vim.api.nvim_tabpage_list_wins(0)
+  local num_buffers = #vim.fn.getbufinfo({ buflisted = 1 })
+
+  if num_windows > 1 then
     vim.cmd('close')
-  else
+  elseif num_buffers > 1 then
     vim.cmd('bdelete')
+  else
+    -- Check if the buffer has more than one line or if the only line is non-empty
+    local line_count = vim.api.nvim_buf_line_count(buf)
+    local has_content = line_count > 1 or (line_count == 1 and vim.api.nvim_buf_get_lines(buf, 0, 1, false)[1] ~= "")
+
+    if has_content then
+      vim.cmd('bdelete')
+    else
+      vim.cmd('q')
+    end
   end
 end
 
