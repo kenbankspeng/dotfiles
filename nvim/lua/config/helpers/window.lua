9fcfe4e485e469afebfa541e4a03408501f918e6
Ken Banks <kenbanks.peng@gmail.com>
2024-08-18 23:20:11 -0400

    smart_close

--- nvim/lua/config/helpers/window.lua
+++ nvim/lua/config/helpers/window.lua
@@ -1,17 +1,38 @@
--- If there are multiple windows, just close the current one
--- Only one window, delete the buffer
 local function smart_close()
   local buf = vim.api.nvim_get_current_buf()
-  local wins = vim.fn.win_findbuf(buf)
   local num_windows = #vim.api.nvim_tabpage_list_wins(0)
   local num_buffers = #vim.fn.getbufinfo({ buflisted = 1 })
 
-  if num_windows > 1 then
+  if num_windows > 2 then
     vim.cmd('close')
   elseif num_buffers > 1 then
+    -- Get a list of all listed buffers and the current window
+    local buffers = vim.fn.getbufinfo({ buflisted = 1 })
+    local current_win = vim.api.nvim_get_current_win()
+    local other_win, other_buf
+
+    -- Find the other window and its buffer
+    for _, win in ipairs(vim.api.nvim_tabpage_list_wins(0)) do
+      if win ~= current_win then
+        other_win = win
+        other_buf = vim.api.nvim_win_get_buf(win)
+        break
+      end
+    end
+
+    -- Find a buffer that is not the current buffer and not the buffer in the other window
+    for _, buffer in ipairs(buffers) do
+      if buffer.bufnr ~= buf and buffer.bufnr ~= other_buf then
+        vim.cmd('buffer ' .. buffer.bufnr)
+        vim.cmd('bdelete ' .. buf)
+        return
+      end
+    end
+
+    -- If we only have 2 buffers and 2 windows, close one window
     vim.cmd('bdelete')
+    vim.cmd('close')
   else
-    -- Check if the buffer has more than one line or if the only line is non-empty
     local line_count = vim.api.nvim_buf_line_count(buf)
     local has_content = line_count > 1 or (line_count == 1 and vim.api.nvim_buf_get_lines(buf, 0, 1, false)[1] ~= "")
 
