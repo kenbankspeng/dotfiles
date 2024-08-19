daf65f931a0c3d94cd0fa9d7d8cd209357a1ab20
Ken Banks <kenbanks.peng@gmail.com>
2024-08-18 23:24:58 -0400

    .

--- nvim/lua/config/helpers/window.lua
+++ nvim/lua/config/helpers/window.lua
@@ -29,9 +29,11 @@ local function smart_close()
       end
     end
 
-    -- If we only have 2 buffers and 2 windows, close one window
-    vim.cmd('bdelete')
-    vim.cmd('close')
+    -- If we only have 2 buffers and 2 windows, close one window and delete the current buffer
+    vim.cmd('bdelete ' .. buf)
+    if num_windows > 1 then
+      vim.cmd('close')
+    end
   else
     local line_count = vim.api.nvim_buf_line_count(buf)
     local has_content = line_count > 1 or (line_count == 1 and vim.api.nvim_buf_get_lines(buf, 0, 1, false)[1] ~= "")
