e05ba68658296c9002c4a0b6f381884c3476784c
Ken Banks <kenbanks.peng@gmail.com>
2024-08-23 11:23:22 -0400

    .

--- nvim/lua/winmgr/select.lua
+++ nvim/lua/winmgr/select.lua
@@ -1,3 +1,31 @@
+local result = {
+  "col",
+  {
+    {
+      "row",
+      {
+        { "leaf", 1000 },
+        { "leaf", 1017 }
+      }
+    },
+    {
+      "row",
+      {
+        { "leaf", 1007 },
+        { "leaf", 1011 }
+      }
+    },
+    {
+      "row",
+      {
+        { "leaf", 1009 },
+        { "leaf", 1010 }
+      }
+    }
+  }
+}
+
+
 return function()
   print(vim.inspect(vim.fn.winlayout()))
   return require('oil').select()
