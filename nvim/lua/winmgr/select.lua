204517184fbe41b269188bc255118b0ccc949114
Ken Banks <kenbanks.peng@gmail.com>
2024-08-23 07:35:36 -0400

    .

--- nvim/lua/winmgr/select.lua
+++ nvim/lua/winmgr/select.lua
@@ -40,13 +40,39 @@ local function find_balanced_split(layout)
 end
 
 -- Function to determine the next split
-local function determine_next_split()
-  local layout = vim.fn.winlayout()
-  return find_balanced_split(layout)
+local function determine_next_split(layout)
+  if layout[1] == "leaf" then
+    return { leaf = layout[2], split = "horizontal" }
+  elseif layout[1] == "row" then
+    local col_leaves = 0
+    for _, sub_layout in ipairs(layout[2]) do
+      if sub_layout[1] == "col" then
+        col_leaves = col_leaves + 1
+      end
+    end
+    if col_leaves < #layout[2] then
+      return { leaf = layout[2][1][2], split = "vertical" }
+    else
+      return find_balanced_split(layout)
+    end
+  elseif layout[1] == "col" then
+    local row_leaves = 0
+    for _, sub_layout in ipairs(layout[2]) do
+      if sub_layout[1] == "row" then
+        row_leaves = row_leaves + 1
+      end
+    end
+    if row_leaves < #layout[2] then
+      return { leaf = layout[2][1][2], split = "horizontal" }
+    else
+      return find_balanced_split(layout)
+    end
+  end
 end
 
 return function()
-  local next_split_info = determine_next_split()
+  local layout = vim.fn.winlayout()
+  local next_split_info = determine_next_split(layout)
   print("Next split leaf: " .. next_split_info.leaf .. " with a " .. next_split_info.split .. " split.")
   -- print(vim.inspect(vim.fn.winlayout()))
   return require('oil').select()
