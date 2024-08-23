1f281d2c76431dfb865b5521ec8db37d3ef9e1d6
Ken Banks <kenbanks.peng@gmail.com>
2024-08-23 10:53:37 -0400

    .

--- nvim/lua/winmgr/select.lua
+++ nvim/lua/winmgr/select.lua
@@ -1,44 +1,4 @@
--- Function to determine the split type based on the current layout
-local function determine_split_type(layout)
-  if layout[1] == "col" then
-    return "horizontal"
-  elseif layout[1] == "row" then
-    return "vertical"
-  end
-  return "horizontal"
-end
-
--- Function to find the best leaf to split
-local function find_best_leaf(layout)
-  if layout[1] == "leaf" then
-    return layout
-  end
-
-  local best_leaf = nil
-  for _, sub_layout in ipairs(layout[2]) do
-    if sub_layout[1] == "leaf" then
-      best_leaf = sub_layout
-      break
-    else
-      best_leaf = find_best_leaf(sub_layout)
-    end
-  end
-
-  return best_leaf
-end
-
--- Function to determine the next split
-local function determine_next_split()
-  local layout = vim.fn.winlayout()
-  local best_leaf = find_best_leaf(layout)
-  local split_type = determine_split_type(layout)
-  print(vim.inspect(layout))
-  return { leaf = best_leaf[2], split = split_type }
-end
-
-
 return function()
-  local result = determine_next_split()
-  print("best leaf: " .. result.leaf .. " split: " .. result.split)
+  print(vim.inspect(vim.fn.winlayout()))
   return require('oil').select()
 end
