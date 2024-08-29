667624d2c5752d46e1c411fe349c24594255f0a9
Ken Banks <kenbanks.peng@gmail.com>
2024-08-29 17:32:13 -0400

    reset root

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -2,14 +2,14 @@ local filecmds = require('neo-tree.sources.filesystem.commands')
 
 local project_root = nil
 local function set_root(state)
-  if not project_root then project_root = state end
+  if not project_root then project_root = state.path end
   filecmds.set_root(state)
 end
 
 -- Function to reset the root to the original path
-local function reset_root(state)
+local function reset_root()
   if project_root then
-    filecmds.set_root(project_root)
+    vim.cmd('Neotree ' .. project_root)
   end
 end
 
