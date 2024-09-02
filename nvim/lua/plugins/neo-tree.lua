77229407f8b2caa5270d7ae5f8eb8953859e98cf
Ken Banks <kenbanks.peng@gmail.com>
2024-09-02 03:39:45 -0400

    .

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -56,13 +56,6 @@ local function open_preview_buffer(filepath)
 
   vim.api.nvim_win_set_buf(preview_win_id, preview_bufnr)
 
-  -- Try renaming the buffer with error handling
-  local success, err = pcall(vim.api.nvim_buf_set_name, preview_bufnr, filepath)
-  if not success then
-    print("Error renaming buffer:", err)
-    return
-  end
-
   -- Set the content of the preview buffer
   vim.api.nvim_buf_set_lines(preview_bufnr, 0, -1, false, vim.fn.readfile(filepath))
 end
