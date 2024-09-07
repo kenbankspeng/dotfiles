b7bdf8140b26802a72d93ef49ac7a26173d8ab72
Ken Banks <kenbanks.peng@gmail.com>
2024-09-07 04:56:28 -0400

    lsp

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -4,15 +4,52 @@ return {
     'neovim/nvim-lspconfig',
     -- will merge with lavyvim config
     dependencies = {
+      { 'nvim-lua/plenary.nvim' },
+
       -- status updates for LSP loading - bottom right corner
-      {
-        'j-hui/fidget.nvim',
-        opts = {}
-      },
+      { 'j-hui/fidget.nvim',    opts = {} },
 
       -- Allows extra capabilities provided by nvim-cmp
       'hrsh7th/cmp-nvim-lsp',
     },
+    opts = function(_, opts)
+      -- Enable the following language servers
+      --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
+      --
+      --  Add any additional override configuration in the following tables. Available keys are:
+      --  - cmd (table): Override the default command used to start the server
+      --  - filetypes (table): Override the default list of associated filetypes for the server
+      --  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
+      --  - settings (table): Override the default settings passed when initializing the server.
+      --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
+
+
+      local more_servers = {
+        -- See `:help lspconfig-all` for a list of all the pre-configured LSPs
+
+        -- lua_ls configured by lazyvim
+        -- lua_ls = {
+        --   settings = {
+        --     Lua = {
+        --       -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
+        --       -- diagnostics = { disable = { 'missing-fields' } },
+        --     },
+        --   },
+        -- },
+
+        marksman = {}
+        -- equivalent to nvim-lspconfig defaults:
+        --   require 'lspconfig'.marksman.setup({
+        --     cmd = { "marksman", "server" },
+        --     filetypes = { "markdown", "markdown.mdx" },
+        --     root_dir = root_pattern(".git", ".marksman.toml"),
+        --     single_file_support = true
+        --   })
+      }
+
+      opts.servers = vim.tbl_deep_extend("force", opts.servers, more_servers)
+      return opts
+    end
     -- config = function()
     --   -- LSP stands for Language Server Protocol. It's a protocol that helps editors
     --   -- and language tooling communicate in a standardized fashion.
@@ -129,41 +166,9 @@ return {
     --     end,
     --   })
 
-    --   -- return config options for setup({options})
+    --
     --   return {
-    --     -- Enable the following language servers
-    --     --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
-    --     --
-    --     --  Add any additional override configuration in the following tables. Available keys are:
-    --     --  - cmd (table): Override the default command used to start the server
-    --     --  - filetypes (table): Override the default list of associated filetypes for the server
-    --     --  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
-    --     --  - settings (table): Override the default settings passed when initializing the server.
-    --     --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
 
-    --     servers = {
-    --       -- See `:help lspconfig-all` for a list of all the pre-configured LSPs
-
-    --       -- lua_ls configured by lazyvim
-    --       -- lua_ls = {
-    --       --   settings = {
-    --       --     Lua = {
-    --       --       -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-    --       --       -- diagnostics = { disable = { 'missing-fields' } },
-    --       --     },
-    --       --   },
-    --       -- },
-
-    --       marksman = {}
-    --       -- equivalent to nvim-lspconfig defaults:
-    --       --   require 'lspconfig'.marksman.setup({
-    --       --     cmd = { "marksman", "server" },
-    --       --     filetypes = { "markdown", "markdown.mdx" },
-    --       --     root_dir = root_pattern(".git", ".marksman.toml"),
-    --       --     single_file_support = true
-    --       --   })
-    --     }
-    --   }
     -- end
   }
 }
--- nvim/lua/plugins/which-key.lua
+++ nvim/lua/plugins/which-key.lua
@@ -18,8 +18,6 @@ return {
   {
     'folke/which-key.nvim',
     event = 'VimEnter', -- Sets the loading event to 'VimEnter'
-    config = function() -- This is the function that runs, AFTER loading
-      require('which-key').setup()
-    end,
+    opts = {}           -- ensure setup is run
   }
 }
