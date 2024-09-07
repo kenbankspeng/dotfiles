ac8807aee4ed006b72ab4d17d51e738e442d79e4
Ken Banks <kenbanks.peng@gmail.com>
2024-09-06 20:36:10 -0400

    .

--- nvim/lua/plugins/markdown-preview.lua
+++ nvim/lua/plugins/markdown-preview.lua
@@ -1,11 +1,10 @@
 return {
   {
-    -- Install markdown preview, use npx if available.
+    -- Install markdown preview, use npm if available.
     -- if this fails, manually install using :call mkdp#util#install()
     "iamcco/markdown-preview.nvim",
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
     ft = { "markdown" },
-    -- build = function() vim.fn["mkdp#util#install"]() end,
     build = function(plugin)
       if vim.fn.executable "npm" then
         vim.cmd("!cd " .. plugin.dir .. " && cd app && npm install")
