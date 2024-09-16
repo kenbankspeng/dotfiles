980a4bd7401d8eb44a463a7d1b253754df385104
Ken Banks <kenbanks.peng@gmail.com>
2024-09-16 06:05:19 -0400

    just, nvim

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -17,7 +17,7 @@ local function parent(state)
   local parent_id = state.tree:get_node():get_parent_id()
   filecmds.navigate_up(state)
 
-  vim.defer_fn(function() require('neo-tree.ui.renderer').focus_node(state, parent_id) end, 40) -- ensure navigate up is finished
+  vim.defer_fn(function() require('neo-tree.ui.renderer').focus_node(state, parent_id) end, 40)   -- ensure navigate up is finished
 end
 
 -- autoclose for neotree float only
@@ -53,7 +53,7 @@ local function open_preview_buffer(filepath)
 
   -- If the preview window doesn't exist, create it
   if preview_winid == nil or not vim.api.nvim_win_is_valid(preview_winid) then
-    vim.cmd('vsplit') -- Open a vertical split for the preview
+    vim.cmd('vsplit')     -- Open a vertical split for the preview
     preview_winid = vim.api.nvim_get_current_win()
   end
 
@@ -119,13 +119,13 @@ return {
       vim.fn.sign_define('DiagnosticSignHint', { text = '󰌵', texthl = 'DiagnosticSignHint' })
 
       require('neo-tree').setup({
-        close_if_last_window = false,   -- Close Neo-tree if it is the last window left in the tab
-        popup_border_style = 'rounded', -- "single" | "double" | "rounded" | "solid" | NC
+        close_if_last_window = false,           -- Close Neo-tree if it is the last window left in the tab
+        popup_border_style = 'rounded',         -- "single" | "double" | "rounded" | "solid" | NC
         enable_git_status = true,
         enable_diagnostics = true,
-        open_files_do_not_replace_types = { 'terminal', 'trouble', 'qf' }, -- when opening files, do not use windows containing these filetypes or buftypes
-        sort_case_insensitive = false,                                     -- used when sorting files and directories in the tree
-        sort_function = nil,                                               -- use a custom function for sorting files and directories in the tree
+        open_files_do_not_replace_types = { 'terminal', 'trouble', 'qf' },         -- when opening files, do not use windows containing these filetypes or buftypes
+        sort_case_insensitive = false,                                             -- used when sorting files and directories in the tree
+        sort_function = nil,                                                       -- use a custom function for sorting files and directories in the tree
         -- sort_function = function (a,b)
         --       if a.type == b.type then
         --           return a.path > b.path
@@ -139,14 +139,14 @@ return {
           },
           indent = {
             indent_size = 2,
-            padding = 1, -- extra padding on left hand side
+            padding = 1,             -- extra padding on left hand side
             -- indent guides
             with_markers = true,
             indent_marker = '│',
             last_indent_marker = '└',
             highlight = 'NeoTreeIndentMarker',
             -- expander config, needed for nesting files
-            with_expanders = nil, -- if nil and file nesting is enabled, will enable expanders
+            with_expanders = nil,             -- if nil and file nesting is enabled, will enable expanders
             expander_collapsed = '',
             expander_expanded = '',
             expander_highlight = 'NeoTreeExpander',
@@ -187,19 +187,19 @@ return {
           -- If you don't want to use these columns, you can set `enabled = false` for each of them individually
           file_size = {
             enabled = true,
-            required_width = 64, -- min width of window required to show this column
+            required_width = 64,             -- min width of window required to show this column
           },
           type = {
             enabled = true,
-            required_width = 122, -- min width of window required to show this column
+            required_width = 122,             -- min width of window required to show this column
           },
           last_modified = {
             enabled = true,
-            required_width = 88, -- min width of window required to show this column
+            required_width = 88,             -- min width of window required to show this column
           },
           created = {
             enabled = true,
-            required_width = 110, -- min width of window required to show this column
+            required_width = 110,             -- min width of window required to show this column
           },
           symlink_target = {
             enabled = false,
@@ -212,11 +212,11 @@ return {
         event_handlers = {
           {
             event = 'file_open_requested',
-            handler = autoclose, -- auto close
+            handler = autoclose,             -- auto close
           },
         },
         window = {
-          position = 'left', -- "left" | "right" | "float"
+          position = 'left',           -- "left" | "right" | "float"
           width = 40,
           mapping_options = {
             noremap = true,
@@ -230,7 +230,7 @@ return {
             ['<left>'] = parent,
             ['<right>'] = set_root,
             ['<cr>'] = preview_enter,
-            ['<esc>'] = 'cancel', -- close preview or floating neo-tree window
+            ['<esc>'] = 'cancel',             -- close preview or floating neo-tree window
             ['P'] = { 'toggle_preview', config = { use_float = true, use_image_nvim = true } },
             -- Read `# Preview Mode` for more information
             ['l'] = 'focus_preview',
@@ -243,23 +243,23 @@ return {
               -- this command supports BASH style brace expansion ("x{a,b,c}" -> xa,xb,xc). see `:h neo-tree-file-actions` for details
               -- some commands may take optional config options, see `:h neo-tree-mappings` for details
               config = {
-                show_path = 'none', -- "none", "relative", "absolute"
+                show_path = 'none',                 -- "none", "relative", "absolute"
               },
             },
-            ['A'] = 'add_directory', -- also accepts the optional config.show_path option like "add". this also supports BASH style brace expansion.
+            ['A'] = 'add_directory',             -- also accepts the optional config.show_path option like "add". this also supports BASH style brace expansion.
             ['d'] = 'delete',
             ['r'] = 'rename',
             ['y'] = 'copy_to_clipboard',
             ['x'] = 'cut_to_clipboard',
             ['p'] = 'paste_from_clipboard',
-            ['c'] = 'copy', -- takes text input for destination, also accepts the optional config.show_path option like "add":
+            ['c'] = 'copy',             -- takes text input for destination, also accepts the optional config.show_path option like "add":
             -- ["c"] = {
             --  "copy",
             --  config = {
             --    show_path = "none" -- "none", "relative", "absolute"
             --  }
             --}
-            ['m'] = 'move', -- takes text input for destination, also accepts the optional config.show_path option like "add".
+            ['m'] = 'move',             -- takes text input for destination, also accepts the optional config.show_path option like "add".
             ['q'] = file_manager.close,
             ['R'] = 'refresh',
             ['?'] = 'show_help',
@@ -271,43 +271,43 @@ return {
         nesting_rules = {},
         filesystem = {
           filtered_items = {
-            visible = false, -- when true, they will just be displayed differently than normal items
+            visible = false,             -- when true, they will just be displayed differently than normal items
             hide_dotfiles = true,
             hide_gitignored = true,
-            hide_hidden = true, -- only works on Windows for hidden files/directories
+            hide_hidden = true,             -- only works on Windows for hidden files/directories
             hide_by_name = {
               --"node_modules"
             },
-            hide_by_pattern = { -- uses glob style patterns
+            hide_by_pattern = {             -- uses glob style patterns
               --"*.meta",
               --"*/src/*/tsconfig.json",
             },
-            always_show = { -- remains visible even if other settings would normally hide it
+            always_show = {             -- remains visible even if other settings would normally hide it
               --".gitignored",
             },
-            always_show_by_pattern = { -- uses glob style patterns
+            always_show_by_pattern = {             -- uses glob style patterns
               --".env*",
             },
-            never_show = { -- remains hidden even if visible is toggled to true, this overrides always_show
+            never_show = {             -- remains hidden even if visible is toggled to true, this overrides always_show
               --".DS_Store",
               --"thumbs.db"
             },
-            never_show_by_pattern = { -- uses glob style patterns
+            never_show_by_pattern = {             -- uses glob style patterns
               --".null-ls_*",
             },
           },
           follow_current_file = {
-            enabled = false,                      -- This will find and focus the file in the active buffer every time
+            enabled = false,                                -- This will find and focus the file in the active buffer every time
             --               -- the current file is changed while the tree is open.
-            leave_dirs_open = false,              -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
+            leave_dirs_open = false,                        -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
           },
-          group_empty_dirs = false,               -- when true, empty folders will be grouped together
-          hijack_netrw_behavior = 'open_default', -- netrw disabled, opening a directory opens neo-tree
+          group_empty_dirs = false,                         -- when true, empty folders will be grouped together
+          hijack_netrw_behavior = 'open_default',           -- netrw disabled, opening a directory opens neo-tree
           -- in whatever position is specified in window.position
           -- "open_current",  -- netrw disabled, opening a directory opens within the
           -- window like netrw would, regardless of window.position
           -- "disabled",    -- netrw left alone, neo-tree does not handle opening dirs
-          use_libuv_file_watcher = false, -- This will use the OS level file watchers to detect changes
+          use_libuv_file_watcher = false,           -- This will use the OS level file watchers to detect changes
           -- instead of relying on nvim autocmd events.
           window = {
             mappings = {
@@ -316,7 +316,7 @@ return {
               ['H'] = 'toggle_hidden',
               ['s'] = 'fuzzy_finder',
               ['D'] = 'fuzzy_finder_directory',
-              ['#'] = 'fuzzy_sorter', -- fuzzy sorting using the fzy algorithm
+              ['#'] = 'fuzzy_sorter',               -- fuzzy sorting using the fzy algorithm
               -- ["D"] = "fuzzy_sorter_directory",
               ['f'] = 'filter_on_submit',
               ['<c-x>'] = 'clear_filter',
@@ -332,7 +332,7 @@ return {
               ['ot'] = { 'order_by_type', nowait = false },
               -- ['<key>'] = function(state) ... end,
             },
-            fuzzy_finder_mappings = { -- define keymaps for filter popup window in fuzzy_finder_mode
+            fuzzy_finder_mappings = {             -- define keymaps for filter popup window in fuzzy_finder_mode
               ['<down>'] = 'move_cursor_down',
               ['<C-n>'] = 'move_cursor_down',
               ['<up>'] = 'move_cursor_up',
@@ -341,15 +341,15 @@ return {
             },
           },
 
-          commands = {}, -- Add a custom command or override a global one using the same function name
+          commands = {},           -- Add a custom command or override a global one using the same function name
         },
         buffers = {
           follow_current_file = {
-            enabled = true,          -- This will find and focus the file in the active buffer every time
+            enabled = true,                      -- This will find and focus the file in the active buffer every time
             --              -- the current file is changed while the tree is open.
-            leave_dirs_open = false, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
+            leave_dirs_open = false,             -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
           },
-          group_empty_dirs = true,   -- when true, empty folders will be grouped together
+          group_empty_dirs = true,               -- when true, empty folders will be grouped together
           show_unloaded = true,
           window = {
             mappings = {
---
+++ nvim/lua/plugins/neophyte.lua
@@ -0,0 +1,70 @@
+-- lazy.nvim example:
+return {
+  {
+    'tim-harding/neophyte',
+    tag = '0.3.0',
+    event = 'VeryLazy',
+    opts = {
+      fonts = {
+        {
+          name = 'Cascadia Code PL',
+          features = {
+            {
+              name = 'calt',
+              value = 1,
+            },
+            -- Shorthand to set a feature to 1
+            'ss01',
+            'ss02',
+          },
+        },
+        -- Shorthand for no features or variations
+        'Symbols Nerd Font',
+        'Noto Color Emoji',
+      },
+      font_size = {
+        kind = 'width', -- 'width' | 'height'
+        size = 10,
+      },
+      -- Multipliers of the base animation speed.
+      -- To disable animations, set these to large values like 1000.
+      cursor_speed = 2,
+      scroll_speed = 2,
+      -- Increase or decrease the distance from the baseline for underlines.
+      underline_offset = 1,
+      -- For transparent window effects, use this to set the default background color.
+      -- This is because most colorschemes in transparent mode unset the background,
+      -- which normally defaults to the terminal background, but we don't have that here.
+      -- You must also pass --transparent as a command-line argument to see the effect.
+      -- Channel values are in the range 0-255.
+      bg_override = {
+        r = 48,
+        g = 52,
+        b = 70,
+        a = 128,
+      },
+    },
+  },
+}
+
+-- Alternatively, the guifont option is supported:
+-- vim.opt.guifont = 'Cascadia Code PL:w10, Symbols Nerd Font, Noto Color Emoji'
+
+-- There are also freestanding functions to set these options as desired:
+
+-- Increase font size
+-- vim.keymap.set('n', '<c-+>', function()
+--   neophyte.set_font_width(neophyte.get_font_width() + 1)
+-- end)
+
+-- Decrease font size
+-- vim.keymap.set('n', '<c-->', function()
+--   neophyte.set_font_width(neophyte.get_font_width() - 1)
+-- end)
+
+-- Use cmd-ctrl-f to toggle fullsreen on MacOS
+-- if this_os:find('Darwin') then
+--  vim.keymap.set('n', '<DC-f>', function()
+--    neophyte.set_fullscreen(not neophyte.get_fullscreen())
+--  end)
+--end
