019c72d79f89e7bfaaabf0de68cdad3c5631f4a8
Ken Banks <kenbanks.peng@gmail.com>
2024-11-24 18:44:59 -0500

    remove yabai

--- sketchybar/plugins/helpers/sketchy.sh
+++ sketchybar/plugins/helpers/sketchy.sh
@@ -1,6 +1,6 @@
 source "$CONFIG_DIR/plugins/helpers/util.sh"
 
-# a quiter sketchybar - add/remove only if exists
+# a quitter sketchybar - add/remove only if exists
 sketchy() {
   local item=$3
   local items=$(sketchybar --query bar | jq -r '.items[]')
