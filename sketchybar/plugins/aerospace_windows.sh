7dc7648bd75859c92e009d9a7afd66f8bb6f7e59
Ken Banks <kenbanks.peng@gmail.com>
2024-11-27 06:20:02 -0500

    ...

--- sketchybar/plugins/aerospace_windows.sh
+++ sketchybar/plugins/aerospace_windows.sh
@@ -6,17 +6,16 @@ source "$PLUGIN_DIR/helpers/aerospace.sh"
 if [ "$SENDER" = "aerospace_workspace_change" ]; then
 
   for sid in $(aerospace_workspaces); do
-    map=$(aerospace_space_window_map | jq --arg space "$sid" '.[] | select(.workspace == $space)')
-    appid=$(echo "$map" | jq -r '.["app-name"]')
+    apps_list=$(aerospace_space_window_map | jq --arg space "$sid" '.[] | select(.workspace == $space)' | jq -r '.["app-name"]')
 
     # if [ "$space_number" = "$CURRENT_SPACE" ]; then
     icon_strip="—"
-    if [ -n "${appid}" ]; then
+    if [ -n "${apps_list}" ]; then
       icon_strip=""
       while read -r app; do
-        icon="$($CONFIG_DIR/icon_map.sh "$appid")"
+        icon="$($CONFIG_DIR/icon_map.sh "$app")"
         icon_strip+="$icon "
-      done <<<"${appid}"
+      done <<<"${apps_list}"
     fi
 
     echo "@@ $icon_strip"
