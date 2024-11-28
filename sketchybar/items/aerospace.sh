99881c3e9bd84d2a4587cf42b8a7904caa2fcda1
Ken Banks <kenbanks.peng@gmail.com>
2024-11-27 19:10:38 -0500

    no space

--- sketchybar/items/aerospace.sh
+++ sketchybar/items/aerospace.sh
@@ -4,19 +4,18 @@ sketchybar --add event aerospace_workspace_change
 
 for sid in $(aerospace_workspaces); do
   props=(
-    space="$sid"
     icon="$sid"
     script="$PLUGIN_DIR/aerospace_space.sh $sid"
     click_script="aerospace workspace $sid"
   )
-  sketchybar --add space space.$sid left \
+  sketchybar --add item space.$sid left \
     --set space.$sid "${props[@]}" \
     --subscribe space.$sid mouse.clicked
 
 done
 
 # reconstruct the bar when a window changes
-sketchybar --add item aerospace_windows left \
-  --set aerospace_windows \
-  script="$PLUGIN_DIR/aerospace_windows.sh" \
-  --subscribe aerospace_windows aerospace_workspace_change
+# sketchybar --add item aerospace_windows left \
+#   --set aerospace_windows \
+#   script="$PLUGIN_DIR/aerospace_windows.sh" \
+#   --subscribe aerospace_windows aerospace_workspace_change
