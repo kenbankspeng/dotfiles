67c03fea038e4c43bcc20375c0f678276e173efa
Ken Banks <kenbanks.peng@gmail.com>
2024-11-27 22:25:05 -0500

    .

--- sketchybar/plugins/aerospace.sh
+++ sketchybar/plugins/aerospace.sh
@@ -4,35 +4,27 @@ source "$CONFIG_DIR/env.sh"
 source "$PLUGIN_DIR/helpers/aerospace.sh"
 source "$PLUGIN_DIR/helpers/sketchy.sh"
 
-if [ "$SENDER" = "aerospace_workspace_change" ]; then
+for sid in $(aerospace_workspaces); do
+  apps_list=$(aerospace_space_window_map | jq --arg space "$sid" '.[] | select(.workspace == $space)' | jq -r '.["app-name"]')
 
-  for sid in $(aerospace_workspaces); do
-    apps_list=$(aerospace_space_window_map | jq --arg space "$sid" '.[] | select(.workspace == $space)' | jq -r '.["app-name"]')
+  icon_strip="—"
+  if [ -n "${apps_list}" ]; then
+    icon_strip=""
+    while read -r app; do
+      icon="$($CONFIG_DIR/icon_map.sh "$app")"
+      icon_strip+="$icon "
+    done <<<"${apps_list}"
+  fi
 
-    echo "$apps_list"
+  props=(
+    icon="$sid"
+    label="$icon_strip"
+    label.font="$ICON_FONT:$ICON_FONTSIZE"
+  )
+  sketchy --add item aerospace.$sid left
+  sketchybar --set aerospace.$sid icon="$sid" label="$icon_strip"
 
-    # if [ "$space_number" = "$CURRENT_SPACE" ]; then
-    icon_strip="—"
-    if [ -n "${apps_list}" ]; then
-      icon_strip=""
-      while read -r app; do
-        icon="$($CONFIG_DIR/icon_map.sh "$app")"
-        icon_strip+="$icon "
-      done <<<"${apps_list}"
-    fi
-
-    props=(
-      icon="$sid"
-      label="$icon_strip"
-      label.font="$ICON_FONT:$ICON_FONTSIZE"
-    )
-    sketchy --add item aerospace.$sid left
-    sketchybar --set aerospace.$sid icon="$sid" label="$icon_strip"
-
-    # fi
-  done
-
-fi
+done
 
 # for sid in $(aerospace_workspaces); do
 #   props=(
