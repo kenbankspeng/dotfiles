574c4490a645ea033f890694b7e1ccc3b9a9f866
Ken Banks <kenbanks.peng@gmail.com>
2024-12-03 22:46:30 -0500

    .

--- sketchybar/plugins/aerospace.sh
+++ sketchybar/plugins/aerospace.sh
@@ -4,25 +4,55 @@ source "$CONFIG_DIR/env.sh"
 source "$PLUGIN_DIR/helpers/aerospace.sh"
 source "$PLUGIN_DIR/helpers/sketchy.sh"
 
+# File to store the last execution time
+LAST_EXECUTION_FILE="/tmp/aerospace_last_execution_time"
+
+# Function to get the last execution time
+get_last_execution_time() {
+  if [ -f "$LAST_EXECUTION_FILE" ]; then
+    cat "$LAST_EXECUTION_FILE"
+  else
+    echo 0
+  fi
+}
+
+# Function to update the last execution time
+update_last_execution_time() {
+  date +%s >"$LAST_EXECUTION_FILE"
+}
+
 # startup - add all workspaces and apps
 if [ "$SENDER" = "forced" ]; then
+  print "$SENDER"
   # aerospace event
   for space in $(aerospace_workspaces); do
     aerospace_add_apps $space
   done
+  update_last_execution_time
 elif [ "$SENDER" = "aerospace_workspace_change" ]; then
+  print "$SENDER"
   # aerospace event
   aerospace_add_apps $PREV_WORKSPACE
   aerospace_add_apps $FOCUSED_WORKSPACE
-elif [ "$SENDER" = "yabai_window_moved" ]; then
-  # yabai event
-  for space in $(aerospace_workspaces); do
-    aerospace_add_apps $space
-  done
-else
+  update_last_execution_time
+elif [ "$SENDER" = "yabai_window_created" ] || [ "$SENDER" = "yabai_window_destroyed" ]; then
+  print "$SENDER"
   # yabai events don't have $PREV_WORKSPACE or $FOCUSED_WORKSPACE
-  #   yabai_window_created
-  #   yabai_window_destroyed
   focused=$(aerospace_focused_workspace)
   aerospace_add_apps $focused
+  update_last_execution_time
+else
+  # yabai_window_moved
+  current_time=$(date +%s)
+  last_execution_time=$(get_last_execution_time)
+  # print "current_time: $current_time"
+  # print "last_execution_time: $last_execution_time"
+  # print "diff: $((current_time - last_execution_time))"
+  # if ((current_time - last_execution_time > 5)); then
+  # print "$SENDER"
+  # for space in $(aerospace_workspaces); do
+  #   aerospace_add_apps $space
+  # done
+  update_last_execution_time
+  # fi
 fi
