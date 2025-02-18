179a19ffeb68c8ce291367f17b34b543bb182def
Ken Banks <kenbanks.peng@gmail.com>
2025-02-18 03:47:56 -0500

    ok

--- sketchybar/plugins/cpu.sh
+++ sketchybar/plugins/cpu.sh
@@ -5,16 +5,13 @@ source "$CONFIG_DIR/env.sh"
 cpuload="$(top -l 2 | grep -E "^CPU" | tail -1 | awk '{ print int($3 + $5 + 0.5)"%" }')"
 
 props=(
-  label.y_offset=0 # undo hack
-	label.padding_left=0 # undo hack
-  label.padding_right=0 # undo hack
-	icon.drawing=off
-	background.height="$LABEL_ONLY_HEIGHT"
   icon="$ICON_CPU"
   icon.color="$SAPPHIRE"
   icon.font="$FONT:$((ICON_FONTSIZE - 2))"
+  label.drawing=on
   label="$cpuload"
   label.color="$SAPPHIRE"
+  label.width=30
 )
 
 if [ -n "$cpuload" ]; then
