35f6f903dd60ca9785687bc57a28c6911d97af00
Ken Banks <kenbanks.peng@gmail.com>
2025-02-24 18:20:42 -0500

    .

--- sketchybar/items/cpu.sh
+++ sketchybar/items/cpu.sh
@@ -4,7 +4,24 @@ source "$PLUGIN_DIR/helpers/sketchy.sh"
 
 location="right"
 
+props=(
+  script="$PLUGIN_DIR/cpu.sh"
+  update_freq=2
+  icon="$ICON_CPU"
+  icon.color="$SAPPHIRE"
+  icon.font="$FONT:$((ICON_FONTSIZE+4))"
+  icon.padding_left=0
+  icon.padding_right=0
+  label.drawing=on
+  label.color="$SAPPHIRE"
+  label.padding_left=10
+  label.padding_right=0
+  label.width=36
+  label.align=center
+  background.padding_left=0
+  background.padding_right=0
+)
+
 sketchy_add_item cpu "$location" \
-  --set cpu update_freq=2 \
-  script="$PLUGIN_DIR/cpu.sh" \
+  --set cpu "${props[@]}" \
   --subscribe cpu mouse.clicked
--- sketchybar/plugins/cpu.sh
+++ sketchybar/plugins/cpu.sh
@@ -5,20 +5,7 @@ source "$CONFIG_DIR/env.sh"
 cpuload="$(top -l 2 | grep -E "^CPU" | tail -1 | awk '{ print int($3 + $5 + 0.5)"%" }')"
 
 props=(
-  icon="$ICON_CPU"
-  icon.color="$SAPPHIRE"
-  icon.font="$FONT:$((ICON_FONTSIZE+4))"
-  label.drawing=on
   label="$cpuload"
-  label.color="$SAPPHIRE"
-  background.padding_left=0
-  background.padding_right=0
-  icon.padding_left=0
-  icon.padding_right=0
-  label.padding_left=10
-  label.padding_right=0
-  label.width=36
-  label.align=center
 )
 
 if [ -n "$cpuload" ]; then
