29799032761312fe9d6c786764cb60e414a346f1
Ken Banks <kenbanks.peng@gmail.com>
2025-03-04 15:36:12 -0500

    widget

--- ubersicht/widgets/weather.widget/lib/temperature/temperature.jsx
+++ ubersicht/widgets/weather.widget/lib/temperature/temperature.jsx
@@ -1,61 +1,12 @@
 import { styled } from "uebersicht";
-// import { colors } from "../theme.js";
-
-// .temperature {
-//   fill: ${colors.text};
-// }
+import { colors } from "../weather-type/theme.js";
 
 const Svg = styled("svg")`
   display: block;
 
-  .outer-ring {
-    fill: #301fff;
+  .scale {
+    fill: ${colors.surface2};
   }
-
-  .sizzling {
-    fill: #ff0000;
-  }
-
-  .hot {
-    fill: #ffa100;
-  }
-
-  .beautiful {
-    fill: #00a900;
-  }
-
-  .pleasant {
-    fill: #00ff00;
-  }
-
-  .tbd {
-    fill: #00c100;
-  }
-
-  .mild {
-    fill: #41e79a;
-  }
-
-  .cool {
-    fill: #0f8ea8;
-  }
-
-  .chilly {
-    fill: #301fff;
-  }
-
-  .cold {
-    fill: #3147ed;
-  }
-
-  .freezing {
-    fill: #8480ff;
-  }
-
-  .frigid {
-    fill: #ddd;
-  }
-
   .gradient {
     fill: url(#a);
   }
@@ -66,62 +17,62 @@ export const Temperature = (props) => {
 		<Svg viewBox="0 0 86 222" {...props}>
 			<title>Temperature</title>
 			<path
-				className="outer-ring"
+				className="scale"
 				d="M319.93 615.367c6.465 4.994 10.631 12.822 10.631 21.615 0 15.067-12.233 27.3-27.3 27.3s-27.3-12.233-27.3-27.3c0-8.559 3.947-16.203 10.118-21.21l.525-152.953c.029-8.173 7.562-14.94 16.975-14.916 9.415.024 16.903 6.832 16.875 15.007zm-4.048 1.007.527-153.478c.021-6.108-5.805-10.93-12.841-10.949-7.037-.018-12.898 4.776-12.919 10.886l-.529 153.935a2.02 2.02 0 0 1-.807 1.61c-5.65 4.245-9.307 11-9.307 18.604 0 12.835 10.42 23.255 23.255 23.255s23.255-10.42 23.255-23.255c0-7.816-3.865-14.736-9.785-18.954a2.02 2.02 0 0 1-.849-1.654"
 				transform="translate(-268.932 -437.99)scale(.98902)"
 			/>
 			<path
-				className="sizzling"
+				className="scale"
 				d="M324.417 606.228h20.16v4.067h-20.16z"
 				transform="translate(-268.416 -591.262)"
 			/>
 			<path
-				className="hot"
+				className="scale"
 				d="M324.417 606.228h20.16v4.067h-20.16z"
 				transform="matrix(.6937 0 0 1 -169.046 -576.228)"
 			/>
 			<path
-				className="beautiful"
+				className="scale"
 				d="M324.417 606.228h20.16v4.067h-20.16z"
 				transform="translate(-268.416 -561.295)"
 			/>
 			<path
-				className="tbd"
+				className="scale"
 				d="M324.417 606.228h20.16v4.067h-20.16z"
 				transform="matrix(.6937 0 0 1 -169.046 -545.87)"
 			/>
 			<path
-				className="pleasant"
+				className="scale"
 				d="M324.417 606.228h20.16v4.067h-20.16z"
 				transform="translate(-268.576 -532.295)"
 			/>
 			<path
-				className="mild"
+				className="scale"
 				d="M324.417 606.228h20.16v4.067h-20.16z"
 				transform="matrix(.6937 0 0 1 -169.206 -516.262)"
 			/>
 			<path
-				className="cool"
+				className="scale"
 				d="M324.417 606.228h20.16v4.067h-20.16z"
 				transform="translate(-268.576 -501.262)"
 			/>
 			<path
-				className="chilly"
+				className="scale"
 				d="M324.417 606.228h20.16v4.067h-20.16z"
 				transform="matrix(.6937 0 0 1 -169.046 -486.262)"
 			/>
 			<path
-				className="cold"
+				className="scale"
 				d="M324.417 606.228h20.16v4.067h-20.16z"
 				transform="translate(-268.576 -471.262)"
 			/>
 			<path
-				className="freezing"
+				className="scale"
 				d="M324.417 606.228h20.16v4.067h-20.16z"
 				transform="matrix(.6937 0 0 1 -169.206 -456.262)"
 			/>
 			<path
-				className="frigid"
+				className="scale"
 				d="M324.417 606.228h20.16v4.067h-20.16z"
 				transform="translate(-268.416 -441.262)"
 			/>
@@ -137,19 +88,18 @@ export const Temperature = (props) => {
 					x1="0"
 					x2="1"
 					y1="0"
-					y2=".004"
-					gradientTransform="matrix(-.43095 132.669 -92.1319 -.55865 85.43 575.664)"
+					y2="0"
+					gradientTransform="matrix(0 164.336 -239.809 0 85 575.664)"
 					gradientUnits="userSpaceOnUse"
 				>
-					<stop offset="0" style={{ stopColor: "red", stopOpacity: 1 }} />
-					<stop offset=".12" style={{ stopColor: "#ffa100", stopOpacity: 1 }} />
-					<stop offset=".22" style={{ stopColor: "#00a900", stopOpacity: 1 }} />
-					<stop offset=".4" style={{ stopColor: "#0f0", stopOpacity: 1 }} />
-					<stop offset=".6" style={{ stopColor: "#2d83b9", stopOpacity: 1 }} />
-					<stop offset=".71" style={{ stopColor: "#2f3aec", stopOpacity: 1 }} />
-					<stop offset=".8" style={{ stopColor: "#301fff", stopOpacity: 1 }} />
-					<stop offset=".89" style={{ stopColor: "#665af4", stopOpacity: 1 }} />
-					<stop offset="1" style={{ stopColor: "#ddd", stopOpacity: 1 }} />
+					<stop offset="0" style={{ stopColor: "#6b001e", stopOpacity: 1 }} />
+					<stop offset=".07" style={{ stopColor: "#ff4000", stopOpacity: 1 }} />
+					<stop offset=".14" style={{ stopColor: "#ffbf00", stopOpacity: 1 }} />
+					<stop offset=".15" style={{ stopColor: "#afa400", stopOpacity: 1 }} />
+					<stop offset=".18" style={{ stopColor: "#036b00", stopOpacity: 1 }} />
+					<stop offset=".42" style={{ stopColor: "#01366c", stopOpacity: 1 }} />
+					<stop offset=".66" style={{ stopColor: "#0401d8", stopOpacity: 1 }} />
+					<stop offset="1" style={{ stopColor: "#b4b4ff", stopOpacity: 1 }} />
 				</linearGradient>
 			</defs>
 		</Svg>
